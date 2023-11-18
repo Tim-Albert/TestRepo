a8df1592413f17dd5286dc6e42f34316ceb190f7
Tim <timalbert.289@gmx.de>
2023-11-18 18:55:29 +0100

    new change

--- CS50's.py
+++ CS50's.py
@@ -1,2 +1,9 @@
-print("Hello, World")
+# Ask user for there name
+print("Whats your name?")
+name = input("name:")
+
+# Hier soll Kommentar eingetragen werden
+print("Hello"+ name)
+
+#New section 
 
